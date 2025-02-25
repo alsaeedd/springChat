c44c896ad7b8c8bf7493061d074a3fdd688d28ac
alsaeedd <saeedalsaeedbusiness@gmail.com>
2025-02-25 14:41:55 +0300

    Setup base project files

---
+++ springChat/src/main/java/com/alsaeed/springChat/SpringChatApplication.java
@@ -0,0 +1,13 @@
+package com.alsaeed.springChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatApplication.class, args);
+	}
+
+}
