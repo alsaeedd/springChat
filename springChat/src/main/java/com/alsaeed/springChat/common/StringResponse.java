c190e1d6ee7213c70902b5c614308dc5c71ce220
alsaeedd <saeedalsaeedbusiness@gmail.com>
2025-02-27 15:18:12 +0300

    Add chat controller, service, repository, and mapper

---
+++ springChat/src/main/java/com/alsaeed/springChat/common/StringResponse.java
@@ -0,0 +1,14 @@
+package com.alsaeed.springChat.common;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StringResponse {
+
+    private String response;
+
+}
